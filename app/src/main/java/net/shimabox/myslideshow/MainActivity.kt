be544b828cd3d555ed500bcf29fa6ceb871e3a85
shimabox <shimabox@gmail.com>
2020-12-26 17:01:52 +0900

    :sparkles: FragmentStateAdapterとViewPager2を関連付ける

--- app/src/main/java/net/shimabox/myslideshow/MainActivity.kt
+++ app/src/main/java/net/shimabox/myslideshow/MainActivity.kt
@@ -2,10 +2,34 @@ package net.shimabox.myslideshow
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentActivity
+import androidx.viewpager2.adapter.FragmentStateAdapter
+import net.shimabox.myslideshow.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    // インナークラス
+    class MyAdapter(fa: FragmentActivity) : FragmentStateAdapter(fa) {
+        private val resources = listOf(
+            R.drawable.slide00, R.drawable.slide01,
+            R.drawable.slide02, R.drawable.slide03,
+            R.drawable.slide04, R.drawable.slide05,
+            R.drawable.slide06, R.drawable.slide07,
+            R.drawable.slide08, R.drawable.slide09
+        )
+
+        override fun getItemCount(): Int = resources.size
+
+        override fun createFragment(position: Int): Fragment
+            = ImageFragment.newInstance(resources[position])
+    }
+
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.pager.adapter = MyAdapter(this)
     }
 }
